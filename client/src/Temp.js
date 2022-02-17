c2b94048494a075b06802d1c696cee858a10bb44
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2022-02-17 14:56:19 +0530

    redirection implemented

---
+++ client/src/Temp.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Grow, Grid, withWidth, Box } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import Posts from './components/Posts/Posts';
+import { getPosts } from './actions/posts';
+import useStyles from './styles';
+import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
+import Exjson from './pages/Exjson';
+
+
+const Temp = ({currentId,currentSF,setCurrentId,setCurrentSF}) => {
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId,currentSF, dispatch]);
+
+  return ( 
+    <>
+    <div>
+    <Container maxWidth="lg">
+      <Header />
+      <Grow in>
+        <Container style={{ margin:"1rem"}}>
+          <div container justify="space-between" alignItems="stretch" spacing={3} >
+            <div item xs={5} sm={7}>
+              <Posts setCurrentId={setCurrentId} setCurrentSF={setCurrentSF} />
+            </div>
+          </div>
+        
+        </Container>
+      </Grow>
+    </Container>
+    </div>
+    </>
+    
+  );
+};
+
+export default Temp;
